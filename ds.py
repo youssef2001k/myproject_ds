e0b886108bf3baf02267a75c7b9a89e0d902b5ef
Ubuntu <youssef@youssefvm.vajc1llisipunjohasruc2htce.cx.internal.cloudapp.net>
2023-11-21 22:07:17 +0000

    on ajoute class livre

--- ds.py
+++ ds.py
@@ -1,4 +1,32 @@
 
+
+
+
+
+class Livre:
+    def __init__(self , titre , auteur , prix , nbpages):
+        self.titre = titre
+        self.auteur = auteur
+        self.prix = prix
+        self.nbpages = nbpages
+        self.proprietaire = None
+        
+    def __str__(self):
+        if self.proprietaire == None:
+            res = " // Livre neuf"
+        else:
+            res = "  // Propriétaire : " + self.proprietaire
+            
+        return "Titre : " + self.titre + "  // Auteur : " + self.auteur + " // Prix : " + str(self.prix) + " // Propriétaire : " + res
+    
+    def acheter(self , proprietaire):
+        if self.proprietaire == None:
+            self.proprietaire = proprietaire
+        else :
+            print("Livre déjà vendu")
+
+
+
 hp = Livre("Harry Potter", "J. K. Rowling", 79.9, 350)
 print(hp)
 print()
@@ -23,3 +51,4 @@ bibrsi2.ajouter(tintin1)
 print()
 bibrsi2.afficher()
 
+ 
